9b2029cdaa2146fa1800d1716b439ed220a98e18
atarahmet6712-dotcom <atarahmet6712@gmail.com>
2026-03-06 19:52:36 +0300

    Cleanup: Finalizing secure distribution repo

--- compile.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-call "C:\Program Files\Microsoft Visual Studio\18\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
-cl /EHsc /MT /O2 loader.cpp /Fe:TitanWare.exe /link urlmon.lib shell32.lib user32.lib gdi32.lib advapi32.lib
-if %ERRORLEVEL% EQU 0 (
-    echo Compilation Successful!
-) else (
-    echo Compilation Failed!
-)
